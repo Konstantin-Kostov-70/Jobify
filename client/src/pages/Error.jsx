a1901c89afbd595706b7575758dd63e5099ca73e
Konstantin-Kostov-70 <kosta.norge@gmail.com>
2024-09-15 08:02:12 +0200

    Content and style was added to Error page

--- client/src/pages/Error.jsx
+++ client/src/pages/Error.jsx
@@ -1,7 +1,34 @@
-const Error = () => {
+import {Link, useRouteError} from 'react-router-dom';
+import Wrapper from '../assets/wrappers/ErrorPage';
+import img from '../assets/images/not-found.svg';
 
+const Error = () => {
+  const error = useRouteError();
+
+  if (error.status === 404) {
+    return (
+      <Wrapper>
+        <div>
+          <img src={img} alt='not-found'/>
+          <h3>
+            Page Not Found
+          </h3>
+          <p>
+            we can`t seem to find the page you are looking for
+          </p>
+          <Link to='/dashboard'>Back Home</Link>
+        </div>
+      </Wrapper>
+    )
+  }
   return (
-     <h1>Error Page</h1>
+     <Wrapper>
+      <div>
+        <h3>
+          Something was wrong!
+        </h3>
+      </div>
+     </Wrapper>
   )
 }
 
