9a38915a07128858dba2c1bfa7bb00b21218ca25
Konstantin-Kostov-70 <kosta.norge@gmail.com>
2024-09-15 06:58:30 +0200

    Content and styles was added to landing page

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,8 +1,34 @@
-
+import Wrapper from "../assets/wrappers/LandingPage";
+import main from "../assets/images/main.svg";
+import { Link } from "react-router-dom";
+import logo from "../assets/images/logo.svg";
 
 const Landing = () => {
   return (
-   <h1>Landing Page</h1>
+    <Wrapper>
+      <nav>
+        <img src={logo} alt="jobify" className="logo" />
+      </nav>
+      <div className="container page">
+        <div className="info">
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>
+            Online job tracker make it easy to track the time spent on projects,
+            tasks and client work. Track the job hours on any device, visualise
+            the progress, budget utilisation and billable hours.
+          </p>
+          <Link to="/register" className="btn register-link">
+            Register
+          </Link>
+          <Link to="/login" className="btn">
+            Login / Demo User
+          </Link>
+        </div>
+        <img src={main} alt="job hunt" className="img main-img" />
+      </div>
+    </Wrapper>
   );
 };
 
