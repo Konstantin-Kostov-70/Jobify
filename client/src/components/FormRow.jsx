c85a96c4e66bf2c102317a875adfaf2c0e067dff
Konstantin-Kostov-70 <kosta.norge@gmail.com>
2024-09-15 22:17:38 +0200

    created FormRow component

---
+++ client/src/components/FormRow.jsx
@@ -0,0 +1,20 @@
+// eslint-disable-next-line react/prop-types
+const FormRow = ({type, name, labelText, defaultValue}) => {
+  return (
+    <div className="form-row">
+      <label htmlFor={name} className="form-label">
+        {labelText || name}
+      </label>
+      <input
+        type={type}
+        id={name}
+        name={name}
+        className="form-input"
+        defaultValue={defaultValue || ''}
+        required="true"
+      />
+    </div>
+  );
+};
+
+export default FormRow;
