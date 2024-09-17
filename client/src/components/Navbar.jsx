d95f581f6c52ef1e4be5a98ea1feb8efcf7b43e6
Konstantin-Kostov-70 <kosta.norge@gmail.com>
2024-09-17 06:01:02 +0200

    created navbar structure

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,11 +1,28 @@
-import Wrapper from '../assets/wrappers/Navbar'
+import Wrapper from "../assets/wrappers/Navbar";
+import { FaAlignLeft } from "react-icons/fa";
+import Logo from "./Logo";
+import {useDashboardContext} from "../pages/DashboardLayout";
 
 const Navbar = () => {
+  const {toggleSidebar} = useDashboardContext();
   return (
-   <Wrapper>
-    Navbar
-   </Wrapper>
-  )
-}
+    <Wrapper>
+      <div className="nav-center">
+        <button 
+        type="button" 
+        className="toggle-btn"
+        onClick={toggleSidebar}
+        >
+          <FaAlignLeft />
+        </button>
+        <div>
+          <Logo />
+          <h4 className="logo-text">Dashboard</h4>
+        </div>
+        <div className="btn-container">toggle/logout</div>
+      </div>
+    </Wrapper>
+  );
+};
 
-export default Navbar
+export default Navbar;
