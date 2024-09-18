212c028ba3a2dc09f47e0bfb45161ef570f2acc0
Konstantin-Kostov-70 <kosta.norge@gmail.com>
2024-09-18 02:21:22 +0200

    ThemeToggle component was created, style and functionality and its was added to navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -15,6 +15,14 @@ import {
   Stats
 } from './pages'
 
+export const checkDefaultTheme = () => {
+  const isDarkTheme = localStorage.getItem('darkTheme') === 'true';
+  document.body.classList.toggle('dark-theme', isDarkTheme);
+  return isDarkTheme
+};
+
+checkDefaultTheme();
+
 const router = createBrowserRouter([
   {
     path: '/',
