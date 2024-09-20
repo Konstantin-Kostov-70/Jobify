c1ff404cf92052f29dd389f208e7f63478937fe2
Konstantin-Kostov-70 <kosta.norge@gmail.com>
2024-09-20 15:32:36 +0200

    params validation was added

--- middleware/validationMiddleware.js
+++ middleware/validationMiddleware.js
@@ -1,26 +1,37 @@
-import { body, validationResult } from "express-validator";
+import { body, param, validationResult } from "express-validator";
 import { BadRequestError } from "../errors/customError.js";
 import { JOB_STATUS, JOB_TYPE } from "../utils/constants.js";
+import mongoose from "mongoose";
 
 const withValidationErrors = (validatesValue) => {
-   return [
+  return [
     validatesValue,
     (req, res, next) => {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-          const errorMessages = errors.array().map((error) => error.msg);
-          throw new BadRequestError(errorMessages)
-        }
-    
-        next();
-      },
-   ]
-}
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        const errorMessages = errors.array().map((error) => error.msg);
+        throw new BadRequestError(errorMessages);
+      }
+
+      next();
+    },
+  ];
+};
 
 export const validateJobInput = withValidationErrors([
-    body("company").notEmpty().withMessage("company is required"),
-    body("position").notEmpty().withMessage("position is required"),
-    body("jobLocation").notEmpty().withMessage("job location is required"),
-    body("jobStatus").isIn(Object.values(JOB_STATUS)).withMessage("invalid status value"),
-    body("jobType").isIn(Object.values(JOB_TYPE)).withMessage("invalid type value"),
-])
+  body("company").notEmpty().withMessage("company is required"),
+  body("position").notEmpty().withMessage("position is required"),
+  body("jobLocation").notEmpty().withMessage("job location is required"),
+  body("jobStatus")
+    .isIn(Object.values(JOB_STATUS))
+    .withMessage("invalid status value"),
+  body("jobType")
+    .isIn(Object.values(JOB_TYPE))
+    .withMessage("invalid type value"),
+]);
+
+export const validateIdParam = withValidationErrors([
+  param("id")
+    .custom((value) => mongoose.Types.ObjectId.isValid(value))
+    .withMessage("invalid MongoDB id"),
+]);
--- routes/jobRouter.js
+++ routes/jobRouter.js
@@ -1,16 +1,23 @@
-import {Router} from 'express';
+import { Router } from "express";
 const router = Router();
 
 import {
-    getAllJobs, 
-    createJob, 
-    getJob, 
-    editJob, 
-    deleteJob
-} from '../controllers/jobControllers.js';
-import { validateJobInput } from '../middleware/validationMiddleware.js';
+  getAllJobs,
+  createJob,
+  getJob,
+  editJob,
+  deleteJob,
+} from "../controllers/jobControllers.js";
+import {
+  validateJobInput,
+  validateIdParam,
+} from "../middleware/validationMiddleware.js";
 
-router.route('/').get(getAllJobs).post(validateJobInput, createJob);
-router.route('/:id').get(getJob).patch(validateJobInput, editJob).delete(deleteJob);
+router.route("/").get(getAllJobs).post(validateJobInput, createJob);
+router
+  .route("/:id")
+  .get(validateIdParam, getJob)
+  .patch(validateJobInput, validateIdParam, editJob)
+  .delete(validateIdParam, deleteJob);
 
 export default router;
