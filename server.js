bbcd4b58c34f1e474be882efb2abeb1d8c301746
Konstantin-Kostov-70 <kosta.norge@gmail.com>
2024-09-20 14:55:53 +0200

    refactoring code

--- server.js
+++ server.js
@@ -3,17 +3,16 @@ import express from "express";
 import morgan from "morgan";
 import * as dotenv from "dotenv";
 import mongoose from "mongoose";
-import {StatusCodes} from "http-status-codes"
+import { StatusCodes } from "http-status-codes";
 
 // MIDDLEWARE
 import errorHandlerMiddleware from "./middleware/errorHandlerMiddleware.js";
 
 // ROUTERS
-import jobRouter from './routes/jobRouter.js'
+import jobRouter from "./routes/jobRouter.js";
 
 dotenv.config();
 const app = express();
-
 
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
@@ -21,20 +20,19 @@ if (process.env.NODE_ENV === "development") {
 
 app.use(express.json());
 
+app.use("/api/v1/jobs", jobRouter);
+
 //TEST ROUTS
 
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-app.use('/api/v1/jobs', jobRouter);
-
-app.use('*', (req, res) => {
-  res.status(404).json({msg: 'not found'})
+app.use("*", (req, res) => {
+  res.status(404).json({ msg: "not found" });
 });
 
 app.use(errorHandlerMiddleware);
-
 
 const port = process.env.PORT || 5100;
 
@@ -47,4 +45,3 @@ try {
   console.log(error);
   process.exit(1);
 }
-
