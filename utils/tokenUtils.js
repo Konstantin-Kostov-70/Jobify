3465ec308e3b7278659b08a5555266cbc86a2206
Konstantin-Kostov-70 <kosta.norge@gmail.com>
2024-09-23 01:10:32 +0200

    created verify utils for JWT

--- utils/tokenUtils.js
+++ utils/tokenUtils.js
@@ -6,3 +6,8 @@ export const createJWT = (payload) => {
     });
     return token;
 }
+
+export const verifyJWT = (token) => {
+  const decoded = jwt.decode(token, process.env.JWT_SECRET);
+  return decoded
+}
