7c40f42dd2a5c8137360ea94d38bbbae9f199285
Konstantin-Kostov-70 <kosta.norge@gmail.com>
2024-09-22 14:22:18 +0200

    added passwordUtils to hash users password

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,11 +1,16 @@
 import { StatusCodes } from "http-status-codes";
-import User from "../models/userModel.js"
+import User from "../models/userModel.js";
+import { hashPassword } from "../utils/passwordsUtils.js";
 
 export const register = async (req, res) => {
+    const hashedPassword = await hashPassword(req.body.password);
+    req.body.password = hashedPassword;
+
     const isFirstAccount = await User.countDocuments() === 0;
     req.body.role = isFirstAccount ? 'admin' : 'user';
+
     const user = await User.create(req.body);
-    res.status(StatusCodes.CREATED).json({ user });
+    res.status(StatusCodes.CREATED).json({ msg: "user created" });
 };
 
 export const login = async (req, res) => {
