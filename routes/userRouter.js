a6da6e2f5f07a3afe4b57b941338a43c035d785e
Konstantin-Kostov-70 <kosta.norge@gmail.com>
2024-09-23 05:15:41 +0200

    authorizePermissions and validateUserInput was added to userRouter

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,9 +1,13 @@
 import { Router } from "express";
 import { getApplicationStats, getCurrentUser, updateUser } from "../controllers/userControllers.js";
+import { validateUserInput } from "../middleware/validationMiddleware.js";
+import { authorizePermissions } from "../middleware/authMiddleware.js";
 const router = Router();
 
-router.get('/current-user', getCurrentUser)
-router.get('/admin/app-stats', getApplicationStats)
-router.patch('/update-user', updateUser)
+router.get('/current-user', getCurrentUser);
+router.patch('/update-user',validateUserInput, updateUser);
+router.get('/admin/app-stats',[
+    authorizePermissions('admin'),getApplicationStats
+]);
 
 export default router
