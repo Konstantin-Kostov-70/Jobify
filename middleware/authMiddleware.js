49fe15fafedf2fb134d3d27b6d6ceaa7f10a0b47
Konstantin-Kostov-70 <kosta.norge@gmail.com>
2024-09-23 05:13:22 +0200

    added authorizePermissions to authMiddleware

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,4 +1,4 @@
-import { UnauthenticatedError } from "../errors/customError.js";
+import { UnauthenticatedError, UnauthorizedError } from "../errors/customError.js";
 import { verifyJWT } from "../utils/tokenUtils.js";
 
 export const authenticateUser = (req, res, next) => {
@@ -12,5 +12,14 @@ export const authenticateUser = (req, res, next) => {
         next()  
     } catch (error) {
         throw new UnauthenticatedError('authentication invalid');
+    }
+}
+
+export const authorizePermissions = (...roles) => {
+    return (req, res, next) => {
+        if (!roles.includes(req.user.role)) {
+            throw new UnauthorizedError('Unauthorize to access this route')
+        }
+        next()
     }
 }
