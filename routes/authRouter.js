2c40f9a4aa2935c6eb258b609a0c0f91ea440091
Konstantin-Kostov-70 <kosta.norge@gmail.com>
2024-09-23 02:35:57 +0200

    Created logout controller and added to the authRouter

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { register, login } from "../controllers/authControllers.js"
+import { register, login, logout } from "../controllers/authControllers.js"
 import { validateRegisterInput, validateLoginInput } from "../middleware/validationMiddleware.js";
 const router = Router();
 
 router.post('/register',validateRegisterInput, register);
 router.post('/login',validateLoginInput, login);
+router.get('/logout', logout);
 
 export default router
