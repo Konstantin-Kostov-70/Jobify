43a439a2201f4cebfea874a75210dee717d7b124
Konstantin-Kostov-70 <kosta.norge@gmail.com>
2024-09-23 05:11:51 +0200

    added functionality to user controllers

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -3,13 +3,23 @@ import User from "../models/userModel.js";
 import Job from "../models/jobModel.js";
 
 export const getCurrentUser = async (req, res) => {
-    res.status(StatusCodes.OK).json('get current user');
-}
-
-export const getApplicationStats = async (req, res) => {
-    res.status(StatusCodes.OK).json('get application stats');
+    const user = await User.findOne({ _id: req.user.userId})
+    const userWithoutPassword = user.toJSON()
+    res.status(StatusCodes.OK).json(userWithoutPassword);
 }
 
 export const updateUser = async (req, res) => {
+    const obj = {...req.body}
+    delete obj.password
+    console.log(obj);
+    
+    const updateUser = await User.findByIdAndUpdate(req.user.userId, obj)
     res.status(StatusCodes.OK).json('user is update');
 }
+
+export const getApplicationStats = async (req, res) => {
+    const users = await User.countDocuments();
+    const jobs = await Job.countDocuments();
+    res.status(StatusCodes.OK).json({ users, jobs });
+}
+
