9dd800e8a696f26496688d91ffad26e4e30330d7
Konstantin-Kostov-70 <kosta.norge@gmail.com>
2024-09-24 05:39:58 +0200

    refactoring main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,9 +1,9 @@
-import "react-toastify/dist/ReactToastify.css";
-import { ToastContainer } from "react-toastify";
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import "react-toastify/dist/ReactToastify.css";
 import './index.css'
+import { ToastContainer } from "react-toastify";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
