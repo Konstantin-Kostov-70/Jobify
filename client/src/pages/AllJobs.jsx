37dcd5f79aca954ead1fba32880275344d6158c8
Konstantin-Kostov-70 <kosta.norge@gmail.com>
2024-09-25 01:05:28 +0200

    added functionality to AllJobs

--- client/src/pages/AllJobs.jsx
+++ client/src/pages/AllJobs.jsx
@@ -1,5 +1,22 @@
+import { JobsContainer, SearchContainer } from "../components";
+import { useLoaderData } from "react-router-dom";
+import { createContext, useContext } from "react";
+
+const AllJobsContext = createContext();
+
 const AllJobs = () => {
-  return <h1>AllJobs Page</h1>
-}
+  const {jobs}  = useLoaderData();
+  
+  return (
+    <AllJobsContext.Provider value={{ jobs }}>
+      <SearchContainer />
+      <JobsContainer />
+    </AllJobsContext.Provider>
+  );
+};
 
 export default AllJobs
+
+export const useAllJobsContext = () => useContext(AllJobsContext);
+
+
