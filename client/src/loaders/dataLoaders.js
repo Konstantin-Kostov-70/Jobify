2652394249e76ce32245f97e08e8502b985a7936
Konstantin-Kostov-70 <kosta.norge@gmail.com>
2024-09-25 02:08:04 +0200

    created editJobLoader

--- client/src/loaders/dataLoaders.js
+++ client/src/loaders/dataLoaders.js
@@ -1,22 +1,33 @@
 import { redirect } from "react-router-dom";
 import customFetch from "../utils/customFetch";
+import { toast } from "react-toastify";
 
 export const dashboardLoader = async () => {
   try {
-    const { data } = await customFetch.get('/users/current-user');
-    return data 
+    const { data } = await customFetch.get("/users/current-user");
+    return data;
   } catch (error) {
     console.log(error?.response?.data?.msg);
-    return redirect('/')
+    return redirect("/");
   }
 };
 
 export const allJobsLoader = async () => {
   try {
-    const { data } = await customFetch.get('/jobs');
-    return data
+    const { data } = await customFetch.get("/jobs");
+    return data;
   } catch (error) {
     console.log(error?.response?.data?.msg);
-    return error
+    return error;
+  }
+};
+
+export const editJobLoader = async ({ params }) => {
+  try {
+    const { data } = await customFetch.get(`/jobs/${params.id}`);
+    return data;
+  } catch (error) {
+    toast.error(error?.response?.data?.msg);
+    return redirect("/dashboard/all-jobs")
   }
 };
