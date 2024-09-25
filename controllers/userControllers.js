719101aa9cf69f7eb9614b86e84aab6ec59214a4
Konstantin-Kostov-70 <kosta.norge@gmail.com>
2024-09-25 15:21:46 +0200

    added cloudinary upload functionality to updateUser controller

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,25 +1,35 @@
-import { StatusCodes } from "http-status-codes"; 
+import { StatusCodes } from "http-status-codes";
 import User from "../models/userModel.js";
 import Job from "../models/jobModel.js";
+import cloudinary from "cloudinary";
+import { promises as fs } from "fs";
 
 export const getCurrentUser = async (req, res) => {
-    const user = await User.findOne({ _id: req.user.userId})
-    const userWithoutPassword = user.toJSON()
-    res.status(StatusCodes.OK).json(userWithoutPassword);
-}
+  const user = await User.findOne({ _id: req.user.userId });
+  const userWithoutPassword = user.toJSON();
+  res.status(StatusCodes.OK).json(userWithoutPassword);
+};
 
 export const updateUser = async (req, res) => {
-    const obj = {...req.body}
-    delete obj.password
-    console.log(obj);
-    
-    const updateUser = await User.findByIdAndUpdate(req.user.userId, obj)
-    res.status(StatusCodes.OK).json('user is update');
-}
+  const newUser = { ...req.body };
+  delete newUser.password;
+
+  if (req.file) {
+    const response = await cloudinary.v2.uploader.upload(req.file.path, { folder: 'jobify' });
+    await fs.unlink(req.file.path);
+    newUser.avatar = response.secure_url;
+    newUser.avatarPublicId = response.public_id;
+  }
+
+  const updatedUser = await User.findByIdAndUpdate(req.user.userId, newUser);
+  if (req.file && updatedUser.avatarPublicId) {
+    await cloudinary.v2.uploader.destroy(updatedUser.avatarPublicId);
+  }
+  res.status(StatusCodes.OK).json("user is update");
+};
 
 export const getApplicationStats = async (req, res) => {
-    const users = await User.countDocuments();
-    const jobs = await Job.countDocuments();
-    res.status(StatusCodes.OK).json({ users, jobs });
-}
-
+  const users = await User.countDocuments();
+  const jobs = await Job.countDocuments();
+  res.status(StatusCodes.OK).json({ users, jobs });
+};
