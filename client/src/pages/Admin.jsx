622531aa7509a6623eae9afc1592b3e1a4c27bcb
Konstantin-Kostov-70 <kosta.norge@gmail.com>
2024-09-25 05:01:41 +0200

    Created admin page functionality and styled it

--- client/src/pages/Admin.jsx
+++ client/src/pages/Admin.jsx
@@ -1,5 +1,29 @@
-const Admin = () => {
-  return <h1>Admin Page</h1>
-}
+import { FaSuitcaseRolling, FaCalendarCheck } from "react-icons/fa";
+import { useLoaderData } from "react-router-dom";
+import Wrapper from "../assets/wrappers/StatsContainer";
+import { StatItem } from "../components";
 
-export default Admin
+const Admin = () => {
+  const { users, jobs } = useLoaderData();
+
+  return (
+    <Wrapper>
+      <StatItem
+        title="current users"
+        count={users}
+        color="#e9b949"
+        bcg="#fcefc7"
+        icon={<FaSuitcaseRolling />}
+      />
+      <StatItem
+        title="total jobs"
+        count={jobs}
+        color="#647acb"
+        bcg="#e0e8f9"
+        icon={<FaCalendarCheck />}
+      />
+    </Wrapper>
+  );
+};
+
+export default Admin;
