b657cb42e6b7bac86ee0ce3b1f3e65f7c6d4f52f
Konstantin-Kostov-70 <kosta.norge@gmail.com>
2024-09-25 02:48:11 +0200

    created editJobAction

--- client/src/actions/fetchActions.js
+++ client/src/actions/fetchActions.js
@@ -44,3 +44,17 @@ export const addJobAction = async ({ request }) => {
   }
 };
 
+export const editJobAction = async ({request, params}) => {
+  const formData = await request.formData();
+  const data = Object.fromEntries(formData);
+ 
+  try {
+    await customFetch.patch(`/jobs/${params.id}`, data);
+    toast.success('Job edited successfully');
+    return redirect('/dashboard/all-jobs');
+  } catch (error) {
+    toast.error(error?.response?.data?.msg);
+    return error;
+  }
+};
+
