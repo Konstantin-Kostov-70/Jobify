5e830267c1ed51b441894edfed0c64c28dad16c6
Konstantin-Kostov-70 <kosta.norge@gmail.com>
2024-09-25 13:53:26 +0200

    added upload function from multer to updateUser router

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -2,10 +2,11 @@ import { Router } from "express";
 import { getApplicationStats, getCurrentUser, updateUser } from "../controllers/userControllers.js";
 import { validateUserInput } from "../middleware/validationMiddleware.js";
 import { authorizePermissions } from "../middleware/authMiddleware.js";
+import upload from "../middleware/multerMiddleware.js";
 const router = Router();
 
 router.get('/current-user', getCurrentUser);
-router.patch('/update-user',validateUserInput, updateUser);
+router.patch('/update-user', upload.single('avatar'), validateUserInput, updateUser);
 router.get('/admin/app-stats',[
     authorizePermissions('admin'),getApplicationStats
 ]);
