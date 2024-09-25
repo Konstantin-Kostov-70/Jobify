cece4272d0a6ba18b092b5f5bc62f304b8afb41c
Konstantin-Kostov-70 <kosta.norge@gmail.com>
2024-09-25 03:09:10 +0200

    created edit job functionality

--- client/src/pages/EditJob.jsx
+++ client/src/pages/EditJob.jsx
@@ -1,5 +1,51 @@
+import { FormRow, FormRowSelect } from "../components";
+import Wrapper from "../assets/wrappers/DashboardFormPage";
+import { useLoaderData } from "react-router-dom";
+import { JOB_STATUS, JOB_TYPE } from "../../../utils/constants";
+import { Form, useNavigation } from "react-router-dom";
+
 const EditJob = () => {
-  return <h1>EditJob Page</h1>
+  const { job } = useLoaderData();
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === "submitting";
+
+  return (
+    <Wrapper>
+      <Form method="post" className="form">
+      <h4 className="form-title">edit job</h4>
+      <div className="form-center">
+          <FormRow type="text" name="position" defaultValue={job.position} />
+          <FormRow type="text" name="company" defaultValue={job.company} />
+          <FormRow
+            type="text"
+            labelText="job location"
+            name="jobLocation"
+            defaultValue={job.jobLocation}
+          />
+
+          <FormRowSelect
+            name="jobStatus"
+            labelText="job status"
+            defaultValue={job.jobStatus}
+            list={Object.values(JOB_STATUS)}
+          />
+          <FormRowSelect
+            name="jobType"
+            labelText="job type"
+            defaultValue={job.jobType}
+            list={Object.values(JOB_TYPE)}
+          />
+          <button
+            type="submit"
+            className="btn btn-block form-btn "
+            disabled={isSubmitting}
+          >
+            {isSubmitting ? "submitting..." : "submit"}
+          </button>
+        </div>
+      </Form>
+    </Wrapper>
+  )
 }
 
 export default EditJob
