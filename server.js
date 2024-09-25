a28ad554acc7d1ebda1f9f285e22a4d825ef42bf
Konstantin-Kostov-70 <kosta.norge@gmail.com>
2024-09-25 13:54:24 +0200

    added cloudinary configuration

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ import * as dotenv from "dotenv";
 import mongoose from "mongoose";
 import { StatusCodes } from "http-status-codes";
 import cookieParser from "cookie-parser";
+import cloudinary from "cloudinary";
 
 //PUBLIC
 import { dirname } from "path";
@@ -22,6 +23,12 @@ import userRouter from "./routes/userRouter.js"
 
 dotenv.config();
 const app = express();
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
+});
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 app.use(express.static(path.resolve(__dirname, "./public")));
