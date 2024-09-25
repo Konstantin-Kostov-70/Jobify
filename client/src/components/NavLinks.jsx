c17f2de6edb6e6c130ab5c58ed015945386480cf
Konstantin-Kostov-70 <kosta.norge@gmail.com>
2024-09-25 03:56:49 +0200

    hide admin icon if user is not admin

--- client/src/components/NavLinks.jsx
+++ client/src/components/NavLinks.jsx
@@ -4,13 +4,15 @@ import { useDashboardContext } from "../pages/DashboardLayout";
 
 // eslint-disable-next-line react/prop-types
 const NavLinks = ({isBigSidebar}) => {
-  const { toggleSidebar } = useDashboardContext();
+  const { toggleSidebar, user } = useDashboardContext();
 
   return (
     <div className="nav-links">
       {links.map((link) => {
         const { text, path, icon } = link;
-
+        const { role } = user
+        
+        if (path === 'admin' && role !== 'admin') return
         return (
           <NavLink
             to={path}
