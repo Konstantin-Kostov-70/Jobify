c049a38dc9579cfb478dae792dbb3715a2d8dfed
Konstantin-Kostov-70 <kosta.norge@gmail.com>
2024-09-26 10:17:24 +0200

    added functionality to statsContainer

--- client/src/components/StatsContainer.jsx
+++ client/src/components/StatsContainer.jsx
@@ -1,8 +1,42 @@
-const StatsContainer = () => {
+/* eslint-disable react/prop-types */
+import { FaSuitcaseRolling, FaCalendarCheck, FaBug } from "react-icons/fa";
+import Wrapper from "../assets/wrappers/StatsContainer";
+import StatItem from "./StatItem";
+
+const StatsContainer = ({ defaultStats }) => {
+  const stats = [
+    {
+      title: "pending applications",
+      count: defaultStats?.pending || 0,
+      icon: <FaSuitcaseRolling />,
+      color: "#f59e0b",
+      bcg: "#fef3c7",
+    },
+    {
+      title: "interviews scheduled",
+      count: defaultStats?.interview || 0,
+      icon: <FaCalendarCheck />,
+      color: "#647acb",
+      bcg: "#e0e8f9",
+    },
+    {
+      title: "jobs declined",
+      count: defaultStats?.declined || 0,
+      icon: <FaBug />,
+      color: "#d66a6a",
+      bcg: "#ffeeee",
+    },
+  ];
+
   return (
-    <div>
-      Stats container
-    </div>
+    <Wrapper>
+      {stats.map((item) => {
+        return <StatItem 
+        key={item.title}
+        {...item}
+        />
+      })}
+    </Wrapper>
   )
 }
 
