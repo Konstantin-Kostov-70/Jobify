83a9e85490b1e9ab508afab2030b4bc623815c8e
Konstantin-Kostov-70 <kosta.norge@gmail.com>
2024-09-27 16:32:22 +0200

    clean test routs

--- server.js
+++ server.js
@@ -44,12 +44,6 @@ app.use("/api/v1/jobs", authenticateUser, jobRouter);
 app.use("/api/v1/users", authenticateUser, userRouter);
 app.use("/api/v1/auth", authRouter);
 
-//TEST ROUTS
-
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
-
 app.use("*", (req, res) => {
   res.status(StatusCodes.NOT_FOUND).json({ msg: "not found" });
 });
